3859958a1ca83de3bf85e278a4198ed463687f24
RonWSO <ronaldwilli6@outlook.com>
2017-11-03 00:43:18 -0300

    Consertado

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleNav() {
+	if(document.getElementById("sidebar").style.width == "150px")
+		document.getElementById("sidebar").style.width = "0";
+	else
+		document.getElementById("sidebar").style.width = "150px";
+}
